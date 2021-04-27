21773f5b654e819bf49ca2cdfffc0f8c0a6c0ca3
lroskoshin <lev_roskoshin@protonmail.com>
2021-04-27 23:49:01 +0300

    added publish script

---
+++ pre-publish.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+echo ">> Cleaning up..."
+rm -rf dist
+
+echo ">> Building a package..."
+npm run build
+
+echo ">> Cleaning up a package.json file..."
+ts-node scripts/clean-package-json.js
+
+echo "Package is ready to publish"
