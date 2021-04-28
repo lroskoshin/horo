058b6c2b6405580d074ab6bf36585a010c4d058a
lroskoshin <lev_roskoshin@protonmail.com>
2021-04-28 15:51:53 +0300

    updated type

--- src/insertions/insertions.ts
+++ src/insertions/insertions.ts
@@ -11,17 +11,17 @@ export interface Component {
     fragment: DocumentFragment;
 }
 
-export type Instertions = Subscribable<string | Component> | Subscription<Event>;
+export type Instertions = Subscribable<string | Component> | Subscription<Event> | string;
 
 export function ensureSubscribable(instertion: Instertions): Subscribable<string | Component> {
-    if('subscribe' in instertion && typeof instertion.subscribe === 'function') {
+    if(typeof instertion !== 'string' && 'subscribe' in instertion && typeof instertion.subscribe === 'function') {
         return instertion;
     } 
     throw new Error('The passed value is not a Subscribable.');
 } 
 
 export function ensureSubscription(instertion: Instertions): Subscription<unknown> {
-    if('next' in instertion && typeof instertion.next === 'function') {
+    if(typeof instertion !== 'string' && 'next' in instertion && typeof instertion.next === 'function') {
         return instertion;
     } 
     throw new Error('The passed value is not a Subscription.');
