2c6f6f208bcb69c7beabb4f3662b0ec7127256ce
lroskoshin <lev_roskoshin@protonmail.com>
2021-04-30 15:35:47 +0300

    changed entrypoint

--- rollup.config.ts
+++ rollup.config.ts
@@ -2,7 +2,7 @@ import ts from '@wessberg/rollup-plugin-ts';
 import { terser } from 'rollup-plugin-terser';
 import pkg from './package.json';
 const config = {
-    input: './src/horo.ts',
+    input: './src/index.ts',
     output: [
         { file: pkg.main, name: 'horo', format: 'umd', sourcemap: true },
         { file: pkg.module, format: 'es', sourcemap: true },
