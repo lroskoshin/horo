eb87351785647ba32fe5cd51b710387d0fc677f3
lroskoshin <lev_roskoshin@protonmail.com>
2021-05-15 10:33:20 +0300

    migrated to comment for insertion

--- src/parser/joiner.ts
+++ src/parser/joiner.ts
@@ -1,12 +1,13 @@
+const eventHandlingPattern = /data-event-([a-z]*)="?$/m;
 export function joiner(template: TemplateStringsArray): string {
     let result = template[0];
     for (let index = 1; index < template.length; index++) {
-        const element = template[index];
-        const dataAttr = result.match(/data-event-([a-z]*)="?$/m);
+        const currentChunk = template[index];
+        const dataAttr = result.match(eventHandlingPattern);
         if(dataAttr !== null) {
-            result =`${result.replace(/data-event-([a-z]*)="?$/m, 'data-event="')}${dataAttr[1]}:${index-1}"${element}`;
+            result =`${result.replace(eventHandlingPattern, 'data-event="')}${dataAttr[1]}:${index-1}"${currentChunk}`;
         } else {
-            result =`${result}<span data-index=${index-1}></span>${element}`;
+            result =`${result}<!--${index-1}-->${currentChunk}`;
         }
     }
     return result;
