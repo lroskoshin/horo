bf275b78acf9ef6600c437641f8297691fb9831d
lroskoshin <lev_roskoshin@protonmail.com>
2021-05-15 12:13:13 +0300

    added unsubscribe reactive value

--- src/insertions/insert-value.ts
+++ src/insertions/insert-value.ts
@@ -1,12 +1,12 @@
 import { injectDynamicValue } from './inject-dynamic-value';
 import { injectStaticValue } from './inject-static-value';
-import { Instertions, ensureValueInsertion, isStationInsertion } from './insertions';
+import { Instertions, ensureValueInsertion, isStationInsertion, Unsubscriber } from './insertions';
 
 type ArrayWithStringIndex<T> = T[] & {
     [index: string]: T;
 }
 
-export function insertValue(fragment: DocumentFragment, insertions: Instertions[]): void {
+export function insertValue(fragment: DocumentFragment, insertions: Instertions[]): Unsubscriber {
     const iterator = document.createTreeWalker(
         fragment, 
         NodeFilter.SHOW_COMMENT,
@@ -14,6 +14,7 @@ export function insertValue(fragment: DocumentFragment, insertions: Instertions[
     // Conversion to array is redundant 
     // Currently there is a conversion due to the replacement of the root comment 
     const nodeList = [];
+    const unsubscribes: Unsubscriber[] = [];
     while(iterator.nextNode()) nodeList.push(iterator.currentNode as Comment);
     for (let i = 0; i < nodeList.length; i++) {
         const socket = nodeList[i];
@@ -22,7 +23,11 @@ export function insertValue(fragment: DocumentFragment, insertions: Instertions[
         if(isStationInsertion(insertion)) {
             injectStaticValue(socket, insertion);
         } else {
-            injectDynamicValue(socket, insertion);
+            unsubscribes.push(injectDynamicValue(socket, insertion));
         }
     }
+
+    return () => {
+        unsubscribes.forEach(u => u());
+    };
 }
