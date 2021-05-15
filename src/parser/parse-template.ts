ea352f55b142c05f84059a7be2490c2a4a5de003
lroskoshin <lev_roskoshin@protonmail.com>
2021-05-15 13:58:38 +0300

    added lint ignore

--- src/parser/parse-template.ts
+++ src/parser/parse-template.ts
@@ -1,6 +1,7 @@
 import { Instertions } from '../insertions/insertions';
 import { joiner } from './joiner';
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function parseTemplate(template: TemplateStringsArray, insertions: Instertions[]): DocumentFragment {
     const root = document.createElement('template');
     root.innerHTML = joiner(template);
