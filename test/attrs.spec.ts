4fed2ac5478bcfa4b4be0288d7443ca59c6e0c81
lroskoshin <lev_roskoshin@protonmail.com>
2021-05-25 18:01:22 +0300

    added static attr test

--- test/attrs.spec.ts
+++ test/attrs.spec.ts
@@ -24,20 +24,27 @@ describe('Reactive Insert RxJS', () => {
 
     beforeAll(() => {
         component = horo`
-        <div data-testid="test" class="${classSubject}">
-            Hello World
-        </div>
+            <div data-testid="reactive" class="${classSubject}">
+                Hello World
+            </div>
+            <div data-testid="static" class="${'baz'}">
+                Hello World
+            </div>
         `;
         element.appendChild(component.fragment);
     });
 
     it('Init Text', () => {
-        expect(getByTestId(element, 'test')).toHaveClass('foo');
+        expect(getByTestId(element, 'reactive')).toHaveClass('foo');
     });
 
     it('Update Text', () => {
         classSubject.next('bar');
-        expect(getByTestId(element, 'test')).toHaveClass('bar');
+        expect(getByTestId(element, 'reactive')).toHaveClass('bar');
+    });
+
+    it('Static attrs', () => {
+        expect(getByTestId(element, 'static')).toHaveClass('baz');
     });
 
     it('Unsubscribe', () => {
