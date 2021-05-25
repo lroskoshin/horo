c5eb92fe1fab6ab4160395d9c683febba9f5f4b1
lroskoshin <lev_roskoshin@protonmail.com>
2021-05-25 18:17:43 +0300

    added  attrs instruction

--- src/instruction.ts
+++ src/instruction.ts
@@ -2,6 +2,8 @@ export const insertionPrefix = 'insert:';
 export const insertionPrefixLength = insertionPrefix.length;
 export const eventHandlingPrefix = 'listen:';
 export const eventHandlingPrefixLength = insertionPrefix.length;
+export const attrsPrefix = 'attrs:';
+export const attrsPrefixLength = insertionPrefix.length;
 
 export function isInsertionInsctruction(instruction: string): boolean {
     return instruction.startsWith(insertionPrefix);
@@ -9,4 +11,8 @@ export function isInsertionInsctruction(instruction: string): boolean {
 
 export function isEventHandlingInstruction(instruction: string): boolean {
     return instruction.startsWith(eventHandlingPrefix);
+}
+
+export function isAttrsInstruction(instruction: string): boolean {
+    return instruction.startsWith(attrsPrefix);
 }
