542ea6d08bc040d75462cf8a8c7572f3ae347097
lroskoshin <lev_roskoshin@protonmail.com>
2021-05-30 21:51:40 +0300

    updated rollup config

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,11 +1,12 @@
 import ts from '@wessberg/rollup-plugin-ts';
+import { RollupOptions } from 'rollup';
 import { terser } from 'rollup-plugin-terser';
 import pkg from './package.json';
-const config = {
+const config: RollupOptions = {
     input: './src/index.ts',
     output: [
         { file: pkg.main, name: 'horo', format: 'umd', sourcemap: true },
-        { file: pkg.module, format: 'es', sourcemap: true },
+        { dir: 'dist', format: 'es', sourcemap: true },
     ],
     plugins: [
         ts(),
