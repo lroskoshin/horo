b4cedffb46bca6b25545b6344800e3f2ff13f66e
lroskoshin <lev_roskoshin@protonmail.com>
2021-05-30 23:38:02 +0300

    added test for merging components

--- test/multiple-component/inject-multiple-component.spec.ts
+++ test/multiple-component/inject-multiple-component.spec.ts
@@ -2,12 +2,29 @@
  * @jest-environment jsdom
  */
  import { getByTestId } from '@testing-library/dom';
+import { horo } from '../../src/horo';
+import { mergeComponents } from '../../src/utils';
 import { mount } from './test-multiple-components';
 
  describe('Inject component', () => {
     const element = document.createElement('div');
     beforeAll(() => {
         mount(element);
+    });
+
+    it('Merge Components', () => {
+        const component = mergeComponents([
+            horo`
+                <span data-testid="foo"> Foo </span>
+            `,
+            horo`
+                <span data-testid="bar"> Bar </span>
+            `
+        ]);
+        const el = document.createElement('div');
+        el.appendChild(component.fragment);
+        expect(getByTestId(el, 'foo')).toHaveTextContent('Foo');
+        expect(getByTestId(el, 'bar')).toHaveTextContent('Bar');
     });
 
     it('Static component insertion', () => {
